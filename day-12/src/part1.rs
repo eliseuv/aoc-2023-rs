9eb64124808d381b2a654dca10ebaf9ba4355e1a
evf <eliseuv816@gmail.com>
2023-12-18 15:23:26 -0300

    update

--- day-12/src/part1.rs
+++ day-12/src/part1.rs
@@ -1,8 +1,90 @@
 use crate::custom_error::AocError;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{self, one_of, space1},
+    multi::{many1, separated_list1},
+    sequence::separated_pair,
+    IResult,
+};
+
+#[derive(Debug, PartialEq, Eq)]
+enum State {
+    Operational,
+    Damaged,
+}
+
+impl State {
+    fn from_char(c: char) -> Option<Self> {
+        match c {
+            '.' => Some(Self::Operational),
+            '#' => Some(Self::Damaged),
+            '?' => None,
+            _ => panic!("Unrecognized character!"),
+        }
+    }
+}
+
+// TODO: Can `Rc<[T]>` be used instead of `Vec<T>`?
+#[derive(Debug)]
+struct DamagedRecord {
+    state: Vec<Option<State>>,
+    grouping: Vec<usize>,
+}
+
+#[derive(Debug)]
+enum GroupState {
+    Operation(usize),
+    Damaged(usize),
+}
+
+#[derive(Debug)]
+struct Record {
+    grouping: Vec<usize>,
+    fill: Vec<usize>,
+}
+
+impl Record {
+    fn new(damaged_record: DamagedRecord) -> Self {
+        let grouping = damaged_record.grouping.clone();
+        let n = grouping.len() + 1;
+        let mut fill = vec![1; n];
+        fill[0] = 0;
+        fill[n - 1] = damaged_record.state.len() - grouping.iter().sum::<usize>() - (n - 1);
+
+        Self { grouping, fill }
+    }
+
+    fn len(&self) -> usize {
+        self.grouping.iter().sum::<usize>() + self.fill.iter().sum::<usize>()
+    }
+
+    fn state(&self) -> Vec<State> {
+        todo!()
+    }
+}
+
+fn parse_record(input: &str) -> IResult<&str, DamagedRecord> {
+    separated_pair(
+        many1(one_of(".#?")),
+        space1,
+        separated_list1(tag(","), complete::u32),
+    )(input)
+    .map(|(input, (springs_chars, grouping))| {
+        (
+            input,
+            DamagedRecord {
+                state: springs_chars.into_iter().map(State::from_char).collect(),
+                grouping: grouping.into_iter().map(|x| x as usize).collect(),
+            },
+        )
+    })
+}
 
 #[tracing::instrument]
 pub fn process_line(line: &str) -> miette::Result<usize, AocError> {
-    todo!("Part 1");
+    let (_, springs) = dbg!(parse_record(line).unwrap());
+
+    Ok(0)
 }
 
 #[tracing::instrument]
@@ -29,7 +111,6 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
         let input = "???.### 1,1,3
 .??..??...?##. 1,1,3
 ?#?#?#?#?#?#?#? 1,3,1,6
